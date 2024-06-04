fd79216c4073c203352790edd282d7f1bfe47df5
Tabitha Fortner <you@example.com>
2024-06-04 13:59:02 -0500

    fizzy

--- JS/assignments/Week2/fizzy/main.js
+++ JS/assignments/Week2/fizzy/main.js
@@ -1,8 +1,29 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+// need function
+// input & output
+// take in number
+// need condition (if num divis by 3 return fizz if by 5 return buzz if both return fizz buzz)
 
+function fizzy(n){
+  if (n % 3 === 0 && n % 5 === 0){
 
+    return "FizzBuzz"
+  
+  } else if ( n % 3 === 0){
+  
+    return "Fizz"
+ 
+  } else if ( n % 5 === 0){
+ 
+    return "Buzz"
+ 
+  } else {
+ 
+    return n 
+ }
+  }
 
 
 /**********************************
